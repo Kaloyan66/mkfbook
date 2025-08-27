8f5989051ff878a09ba646adb79d97f49568d660
Kaloyan66 <kaloqnv5@gmail.com>
2025-08-27 20:10:45 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,13 @@ import App from './App';
 import Success from './pages/Success';
 import PaymentSuccess from './pages/PaymentSuccess';
 import './index.css';
+import Cancel from './pages/Cancel';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/success',
-    element: <Success />,
-  },
-  {
-    path: '/payment-success',
-    element: <PaymentSuccess />,
-  },
+  { path: '/', element: <App /> },
+  { path: '/success', element: <Success /> },
+  { path: '/payment-success', element: <PaymentSuccess /> },
+  { path: '/cancel', element: <Cancel /> },
 ]);
 
 createRoot(document.getElementById('root')!).render(
